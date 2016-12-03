8513867edc327c318db9e02e804e8f58a87fbfc9
Tarek Saier <tareksaier@gmail.com>
2016-12-03 23:11:28 +0100

    fixed 2ch-keshou

--- 2ch-keshou.user.js
+++ 2ch-keshou.user.js
@@ -8,11 +8,11 @@
 var firstThreadLinks = document.querySelectorAll('a[href="#1"]');
 var firstThreadLink  = firstThreadLinks[1];  // [0] is a link at the page top
 var threadListCntr   = firstThreadLink.parentNode;
-var threadList       = threadListCntr.querySelectorAll('a');
+var threadList       = threadListCntr.querySelectorAll('font > a');
 
 // add space for uniform indent
 var sp = document.createTextNode('　');
-threadListCntr.insertBefore(sp, threadList[1]);
+threadListCntr.insertBefore(sp, threadList[0]);
 
 for (var i=0; i<threadList.length; i++) {
     threadList[i].style.setProperty('font-size',       '16px')
@@ -20,7 +20,7 @@ for (var i=0; i<threadList.length; i++) {
     threadList[i].style.setProperty('display',         'inline-block')
     threadList[i].style.setProperty('margin',          '2px')
 
-    if (i<20 && i%2==1) continue; // jump seperated top 10 anchor links
+    if (i<20 && i%2==0) continue; // jump seperated top 10 anchor links
     var br = document.createElement('br');
     threadListCntr.insertBefore(br, threadList[i].nextSibling)
     }
