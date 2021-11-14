27a4f1e41f7ae686f43fdb520f8223b49d7c2ac3
Tarek Saier <tareksaier@gmail.com>
2021-11-14 11:43:35 +0100

    add variable for furigana size in enlargeJapanese

--- enlargeJapanese.user.js
+++ enlargeJapanese.user.js
@@ -6,16 +6,17 @@
 // ==/UserScript==
 
 var minFontSize = 16;
+var minFontSizeFurigana = minFontSize/2;
 var jPatt = /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uffef\u4e00-\u9faf\u3400-\u4dbf]+/g;
 
 /*
  * http://salaciak.blogspot.de/2011/02/javascript-dom-how-to-get-elements.html
  */
 function elementCurrentStyle(element, styleName){
-    if (element.currentStyle){
+    if (element.currentStyle) {
         var i = 0, temp = "", changeCase = false;
-        for (i = 0; i < styleName.length; i++)
-            if (styleName[i] != '-'){
+        for (i = 0; i < styleName.length; i++) {
+            if (styleName[i] != '-') {
                 temp += (changeCase ? styleName[i].toUpperCase() : styleName[i]);
                 changeCase = false;
             } else {
@@ -23,6 +24,7 @@ function elementCurrentStyle(element, styleName){
             }
         styleName = temp;
         return element.currentStyle[styleName];
+        }
     } else {
         return getComputedStyle(element, null).getPropertyValue(styleName);
     }
@@ -32,11 +34,11 @@ function elementCurrentStyle(element, styleName){
  * sirtetris
  */
 
-function getEnlarged(text) {
-        return text.replace(jPatt, '<span style="font-size:'+minFontSize+'px !important;">$&</span>');
+function getEnlarged(text, fontSize) {
+        return text.replace(jPatt, '<span style="font-size:'+fontSize+'px !important;">$&</span>');
     }
 
-function enlargeOnlyInnerMost(elem) {
+function enlargeOnlyInnerMost(elem, fontSize) {
     if(elem.nodeName == "TEXTAREA") return;
     if(elem.hasChildNodes()) {
         var cn = elem.childNodes;
@@ -57,19 +59,28 @@ function enlargeOnlyInnerMost(elem) {
             }
         if(elem.nodeType == 3) {
             // a text node
-            var paret = elem.parentNode;
-            if(elementCurrentStyle(paret, "font-size").replace("px","") >= minFontSize) {
-                return;
+            var parent = elem.parentNode;
+            var setFontSize = minFontSize;
+            if (parent.tagName == 'RT') {
+                if(elementCurrentStyle(parent, "font-size").replace("px","") >= minFontSizeFurigana) {
+                    return;
                 }
-            if(paret.childNodes.length == 1) {
+                setFontSize = minFontSizeFurigana;
+            }
+            else {
+                if(elementCurrentStyle(parent, "font-size").replace("px","") >= minFontSize) {
+                    return;
+                }
+            }
+            if(parent.childNodes.length == 1) {
                 // a node with only text in it. it's save to just insert < and >
-                paret.innerHTML = getEnlarged(paret.innerHTML);
+                parent.innerHTML = getEnlarged(parent.innerHTML, setFontSize);
                 }
             else {
                 // node with tags in it. be sure to not insert < and > inside of them
                 var toDo = (elem.nodeValue.match(jPatt) || Array());
-                for(var i=0; i<toDo.length; i++) {
-                    paret.innerHTML = paret.innerHTML.replace(toDo[i], '<span style="font-size:'+minFontSize+'px !important;">'+toDo[i]+'</span>');
+                for(var j=0; i<toDo.length; j++) {
+                    parent.innerHTML = parent.innerHTML.replace(toDo[i], '<span style="font-size:'+setFontSize+'px !important;">'+toDo[i]+'</span>');
                     }
                 }
             }
